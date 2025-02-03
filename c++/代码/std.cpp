f15c6ec0fd15a28d490eaf9e3178875b27c133bd
cqbzkrx <creesh114514@gmail.com>
2025-02-03 18:16:52 +0800

    modified:   "c++/\344\273\243\347\240\201/std.cpp"

--- c++/代码/std.cpp
+++ c++/代码/std.cpp
@@ -196,14 +196,6 @@ void init () {
 }
 
 void solve () {
-    int n; cin >> n;
-    vector a(n + 1, vector (n + 1, 0));
-    for (int i = 1; i <= n; i++) for (int j = 1; j <= n; j++)
-        cin >> a[i][j];
-    
-    for (int i = 1; i <= n; i++) for (int j = n - 1; j >= 1; j--)
-        a[i][j] += a[i][j + 1];
-
     
 }
 
@@ -222,6 +214,6 @@ int main () {
     cerr << setprecision(2) << fixed;
 
     int t = 1;
-    cin >> t;
+    // cin >> t;
     while (t--) solve ();
 }
