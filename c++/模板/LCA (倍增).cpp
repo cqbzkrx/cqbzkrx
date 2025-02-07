2c9045a315f05db081f34f07844ee6f041546812
cqbzkrx <creesh114514@gmail.com>
2025-02-07 12:08:37 +0800

    Update LCA (倍增).cpp

--- c++/模板/LCA (倍增).cpp
+++ c++/模板/LCA (倍增).cpp
@@ -2,20 +2,22 @@ class LCA {
     void dfs (int v, int fa) {
         dep[v] = dep[fa] + 1;
         f[0][v] = fa;
-
-        for (auto &u : e[v]) if (u != fa)
+        for (auto &[u, w] : e[v]) if (u != fa)
             dfs (u, v);
     }
 
     public:
 
-    static constexpr int N = 4e5 + 5;
+    static constexpr int N = 1e4 + 5;
     int n, lim;
-    vector <vector <int>> f, e;
+    vector <vector <int>> f;
+    vector <vector <pair <int, ll>>> e;
     vector <int> dep;
+    vector <ll> s;
     
-    LCA () : n(0), lim(0), f(vector (__lg(N) + 1, vector (N, 0))), e(vector (N, vector (0, 0))), dep(N, 0) {}
-    LCA (int sz, int rt) {init (sz, rt);}
+    LCA () : n(0), lim(0), f(vector (__lg(N) + 1, vector (N, 0))),
+            e(vector (N, vector (0, pair (0, 0ll)))), dep(N, 0) {}
+    LCA (int sz, int rt, const vector <vector <pair <int, ll>>> &a) {init (sz, rt, a);}
     
     int lca (int v, int u) {
         if (dep[v] > dep[u]) swap (v, u);
@@ -32,8 +34,12 @@ class LCA {
         return f[0][u];
     }
 
-    void init (int sz, int rt) {
+    void init (int sz, int rt, const vector <vector <pair <int, ll>>> &a) {
+        e = a;
+        f = vector (__lg(N) + 1, vector (N, 0));
+        dep = vector (N, 0);
         n = sz, lim = __lg(sz);
+
         dep[rt] = 1; dfs (rt, 0);
         for (int i = 1; i <= lim; i++) for (int j = 1; j <= n; j++)
             f[i][j] = f[i - 1][f[i - 1][j]];
