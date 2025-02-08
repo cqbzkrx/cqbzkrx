1c5c76fce8f4ec7d4afdbdf6e0ead7797e642f0f
cqbzkrx <creesh114514@gmail.com>
2025-02-08 09:23:16 +0800

    Update LCA (倍增+树上前缀和).cpp

--- c++/模板/LCA (倍增+树上前缀和).cpp
+++ c++/模板/LCA (倍增+树上前缀和).cpp
@@ -19,7 +19,7 @@ class LCA {
     
     LCA () : n(0), lim(0), f(vector (__lg(N) + 1, vector (N, 0))),
             e(vector (N, vector (0, pair (0, 0ll)))), dep(N, 0), s(vector (N, 0ll)) {}
-    LCA (int sz, int rt, const vector <vector <pair <int, ll>>> &a) {init (sz, rt, a);}
+    LCA (int sz, int rt, const vector <vector <pair <int, ll>>> &a) {clear (), init (sz, rt, a);}
     
     int lca (int v, int u) {
         if (dep[v] > dep[u]) swap (v, u);
@@ -38,14 +38,17 @@ class LCA {
 
     void init (int sz, int rt, const vector <vector <pair <int, ll>>> &a) {
         e = a;
-        f = vector (__lg(N) + 1, vector (N, 0));
-        dep = vector (N, 0);
-        s = vector (N, 0ll);
         n = sz, lim = __lg(sz);
 
         dep[rt] = 1; dfs (rt, 0);
         for (int i = 1; i <= lim; i++) for (int j = 1; j <= n; j++)
             f[i][j] = f[i - 1][f[i - 1][j]];
+    }
+
+    void clear () {
+        f.clear(), f = vector (__lg(N) + 1, vector (N, 0));
+        dep.clear(), dep = vector (N, 0);
+        s.clear(), s = vector (N, 0ll);
     }
 
     ll len (int v, int u) {
