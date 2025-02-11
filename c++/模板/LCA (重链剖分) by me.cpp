8680f40c4b7dd757e8e400143aad6a3d8116b59e
cqbzkrx <creesh114514@gmail.com>
2025-02-11 21:14:24 +0800

    Update LCA (重链剖分) by me.cpp

--- c++/模板/LCA (重链剖分) by me.cpp
+++ c++/模板/LCA (重链剖分) by me.cpp
@@ -1,12 +1,12 @@
-class LCA {
+class HPD {
 public:
     int cnt, n;
     vector <int> son, siz, f, top, dep, out, dfn;
     vector <vector <int>> e;
     
-    LCA () : cnt(0), n(0), son(vector (0, 0)), siz(vector (0, 0)), f(vector (0, 0)), top(vector (0, 0)), dep(vector (0, 0)),
+    HPD () : cnt(0), n(0), son(vector (0, 0)), siz(vector (0, 0)), f(vector (0, 0)), top(vector (0, 0)), dep(vector (0, 0)),
             out(vector (0, 0)), dfn(vector (0, 0)), e(vector (0, vector (0, 0))) {}
-    LCA (int sz, int rt, const vector <vector <int>> &a) {init (sz, rt, a);}
+    HPD (int sz, int rt, const vector <vector <int>> &a) {init (sz, rt, a);}
 
     void dfs1 (int v, int fa) {
         f[v] = fa;
@@ -40,7 +40,7 @@ public:
         dfs2 (rt, rt);
     }
 
-    int lca (int v, int u) {
+    int HPD (int v, int u) {
         while (top[v] != top[u])
             if (dep[top[v]] < dep[top[u]]) u = f[top[u]];
             else v = f[top[v]];
