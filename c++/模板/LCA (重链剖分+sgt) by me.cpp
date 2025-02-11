03ff07ec9e96c10b776fd89d7daedbf54a06200b
cqbzkrx <creesh114514@gmail.com>
2025-02-11 19:45:05 +0800

    Create LCA (重链剖分+sgt) by me.cpp

---
+++ c++/模板/LCA (重链剖分+sgt) by me.cpp
@@ -0,0 +1,80 @@
+template <typename T>
+class LCA {
+public:
+    int cnt, n;
+    vector <int> son, siz, f, top, dep, rnk, dfn;
+    vector <vector <int>> e;
+    vector <T> s;
+    sgt_lazy <T> sgt;
+    
+    LCA () : cnt(0), n(0), son(vector (0, 0)), siz(vector (0, 0)), f(vector (0, 0)), top(vector (0, 0)), dep(vector (0, 0)),
+            rnk(vector (0, 0)), dfn(vector (0, 0)), e(vector (0, vector (0, 0))), s(vector (0, T(0))) {}
+    LCA (int sz, int rt, const vector <vector <int>> &a, const vector <T> &c) {init (sz, rt, a, c);}
+
+    void dfs1 (int v, int fa) {
+        f[v] = fa;
+        dep[v] = dep[fa] + 1;
+        siz[v] = 1, son[v] = -1;
+        
+        int maxn = 0;
+        for (auto &u : e[v]) if (u != fa) {
+            dfs1 (u, v);
+            siz[v] += siz[u];
+
+            if (siz[u] > maxn) maxn = siz[u], son[v] = u;
+        }
+    }
+
+    void dfs2 (int v, int head) {
+        top[v] = head;
+        dfn[v] = ++cnt;
+        rnk[cnt] = v;
+        if (son[v] == -1) return ;
+        dfs2 (son[v], head);
+        for (auto &u : e[v]) if (u != son[v] && u != f[v])
+            dfs2 (u, u);
+    }
+
+    void init (int sz, int rt, const vector <vector <int>> &a, const vector <T> &c) {
+        n = sz, e = a, cnt = 0;
+        son = siz = f = top = dep = rnk = dfn = vector (n + 1, 0);
+        s.resize(n + 1);
+
+        dep[rt] = 1;
+        dfs1 (rt, 0);
+        dfs2 (rt, rt);
+
+        for (int i = 1; i <= n; i++) s[i] = c[rnk[i]];
+        sgt.init (s);
+    }
+
+    int lca (int v, int u) {
+        while (top[v] != top[u]) {
+            if (dep[top[v]] < dep[top[u]]) u = f[top[u]];
+            else v = f[top[v]];
+        }
+        return (dep[u] > dep[v] ? v : u);
+    }
+
+    T qry (int v, int u) {
+        T ans = 0;
+        while (top[v] != top[u]) {
+            if (dep[top[v]] < dep[top[u]]) ans += sgt.qry (dfn[top[u]], dfn[u]).ans, u = f[top[u]];
+            else ans += sgt.qry (dfn[top[v]], dfn[v]).ans, v = f[top[v]];
+        }
+        
+        if (dep[v] > dep[u]) swap (v, u);
+        ans += sgt.qry (dfn[v], dfn[u]).ans;
+        return ans;
+    }
+
+    void modify (int v, int u, T x) {
+        while (top[v] != top[u]) {
+            if (dep[top[v]] < dep[top[u]]) sgt.modify (dfn[top[u]], dfn[u], x), u = f[top[u]];
+            else sgt.modify (dfn[top[v]], dfn[v], x), v = f[top[v]];
+        }
+        
+        if (dep[v] > dep[u]) swap (v, u);
+        sgt.modify (dfn[v], dfn[u], x);
+    }
+};
