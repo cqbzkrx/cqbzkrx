10e5d05e437e7575ad26e6bc7884f6de291e7e2a
cqbzkrx <creesh114514@gmail.com>
2025-02-11 12:13:42 +0800

    Update LCA (TarJan).cpp

--- c++/模板/LCA (TarJan).cpp
+++ c++/模板/LCA (TarJan).cpp
@@ -1,24 +1,32 @@
 class LCA_tarjan {
-    void init_dsu (int n) {
-        fa.resize(n + 1);
-        iota (all(fa), 0);
-    }
+    class DSU_of_LCA {
+    public:
+        vector <int> fa;
+        void init (int n) {
+            fa.resize(n + 1);
+            iota (all(fa), 0);
+        }
 
-    int find (int x) {
-        return fa[x] == x ? x : fa[x] = find(fa[x]);
-    }
+        int find (int x) {
+            return fa[x] == x ? x : fa[x] = find(fa[x]);
+        }
 
-    void merge (int v, int u) {
-        auto fv = find(v), fu = find(u);
-        if (fv == fu) return ;
-        fa[fv] = fu;
-    }
+        void merge (int v, int u) {
+            auto fv = find(v), fu = find(u);
+            if (fv == fu) return ;
+            fa[fv] = fu;
+        }
+
+        DSU_of_LCA () : fa(vector (0, 0)) {}
+        DSU_of_LCA (int n) {init (n);}
+    };
 
 public:
     vector <vector <int>> e;
     vector <vector <pair <int, int>>> eq;
-    vector <int> vis, ans, fa;
+    vector <int> vis, ans;
     int n, q;
+    DSU_of_LCA dsu;
     
     LCA_tarjan () : n(0), q(0) {}
     LCA_tarjan (int sz, int k) {init (sz, k);}
@@ -26,18 +34,18 @@ public:
     void dfs (int v, int fa) {
         for (auto &u : e[v]) if (u != fa) {
             dfs (u, v);
-            merge (u, v);
+            dsu.merge (u, v);
         }
 
         vis[v] = 1;
 
         for (auto &[u, idx] : eq[v]) if (vis[u])
-            ans[idx] = find (u);
+            ans[idx] = dsu.find (u);
     }
 
     void init (int sz, int k) {
         n = sz, q = k;
-        init_dsu (n);
+        dsu.init (n);
         vis.resize(sz + 1);
         ans.resize(k);
         eq.resize(sz + 1);
