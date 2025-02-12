c36f94d0e33149f12833b773fa532629c7433993
cqbzkrx <creesh114514@gmail.com>
2025-02-12 16:38:47 +0800

    Update dijkstra (最短路).cpp

--- c++/模板/dijkstra (最短路).cpp
+++ c++/模板/dijkstra (最短路).cpp
@@ -1,30 +1,22 @@
-template <class info>
-class dijkstra {
-public:
-    vector <vector <info>> e;
-    vector <int> dis, vis;
+struct Node {
+    int v; ll w;
+    Node () : v(0), w(0ll) {}
+    Node (int x, ll y) {v = x, w = y;}
+};
 
-    void dij (int s) {
-        fill(all(dis), inf); dis[s] = 0;
-        fill(all(vis), 0);
-        priority_queue <pair <int, int>, vector <pair <int, int>>, greater <pair <int, int>>> pq;
-        pq.push({0, s});
-        while(pq.size()) {
-            auto u = pq.top().second; pq.pop();
-            if (vis[u]) continue; vis[u] = 1;
-            for (auto &i : e[u]) if (dis[u] + i.w < dis[i.v]) {
-                dis[i.v] = dis[u] + i.w;
-                pq.push({dis[i.v], i.v});
-            }
+vector dis(n + 1, 0ll), vis(n + 1, 0);
+
+void dij (int s) {
+    fill(all(dis), INF); dis[s] = 0ll;
+    fill(all(vis), 0);
+    priority_queue <pair <ll, int>, vector <pair <ll, int>>, greater <pair <ll, int>>> pq;
+    pq.push({0ll, s});
+    while(pq.size()) {
+        auto u = pq.top().second; pq.pop();
+        if (vis[u]) continue; vis[u] = 1;
+        for (auto &[v, w] : e[u]) if (dis[u] + w < dis[v]) {
+            dis[v] = dis[u] + w;
+            pq.push({dis[v], v});
         }
     }
-
-    dijkstra () : dis(vector (0, 0)), vis(vector (0, 0)) {}
-    dijkstra (int n, const vector <vector <info>> &a) {dis = vector (n + 1, 0), vis = vector (n + 1, 0), e = a;}
-};
-
-struct Node {
-    int v, w;
-    Node () : v(0), w(0) {}
-    Node (int x, int y) {v = x, w = y;}
-};
+}
