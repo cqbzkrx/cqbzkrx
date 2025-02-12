46e0418690f05d38d89fdcd295ada6e721375ca0
cqbzkrx <creesh114514@gmail.com>
2025-02-12 21:48:58 +0800

    modified:   "c++/\344\273\243\347\240\201/.cph/.std.cpp_32b9931b54c5d8a2bbe612c85e902032.prob" 	modified:   "c++/\344\273\243\347\240\201/std.cpp"

--- c++/代码/std.cpp
+++ c++/代码/std.cpp
@@ -9,6 +9,7 @@ const ll INF = 1e18 + 7;
 const db eps = 1e-6;
 const int mod = 1e9 + 7;
 const int N = 2e6 + 7;
+const int M = 2e3 + 7;
 
 namespace K {
     namespace Hash {
@@ -109,14 +110,158 @@ namespace K {
     }
 
     namespace exstd {
-        
+        template <typename T>
+        class sgt_lazy {
+        protected:
+            static constexpr int rt = 1;
+            static constexpr T INVALID = -INF;
+
+            template <typename T1>
+            struct Node {
+                T1 ans, l, r, lazy;
+                Node () : ans(0), l(INVALID), r(INVALID), lazy(INVALID) {}
+                Node (T1 as, T1 cl = INVALID, T1 cr = INVALID, T1 inv = INVALID) {ans = as, l = cl, r = cl, lazy = inv;}
+
+                Node operator + (const Node &a) const {
+                    Node ret;
+                    ret.ans = a.ans + ans - (a.r == l);
+                    ret.l = a.l; ret.r = r;
+                    return ret;
+                }
+            };
+
+            int n;
+            vector <Node <T>> t;
+
+            void build (const vector <T> &a, int p, int cl, int cr) {
+                if (cl == cr) {t[p] = Node <T> (1, a[cl], a[cl]); return ;}
+                auto lc = p << 1, rc = lc | 1, mid = (cl + cr) >> 1;
+                build (a, lc, cl, mid); build (a, rc, mid + 1, cr);
+                t[p] = t[lc] + t[rc];
+            }
+
+            void modify (int l, int r, T x, int p, int cl, int cr) {
+                if (cl > r || cr < l) return ;
+                if (cl >= l && cr <= r) {merge (t[p], x, cr - cl + 1); return ;}
+                push_down (p, cl, cr);
+                auto lc = p << 1, rc = lc | 1, mid = (cl + cr) >> 1;
+                modify (l, r, x, lc, cl, mid);
+                modify (l, r, x, rc, mid + 1, cr);
+                t[p] = t[lc] + t[rc];
+            }
+
+            Node qry (int l, int r, int p, int cl, int cr) {
+                if (cl > r || cr < l) return Node (0);
+                if (cl >= l && cr <= r) return t[p];
+                push_down (p, cl, cr);
+                auto lc = p << 1, rc = lc | 1, mid = (cl + cr) >> 1;
+                return qry (l, r, lc, cl, mid) + qry (l, r, rc, mid + 1, cr);
+            }
+
+            void merge (Node &s, T x, int len = 1) {
+                s.ans = 1;
+                s.lazy = s.l = s.r = x;
+            }
+
+            void push_down (int p, int cl, int cr) {
+                if (cl == cr || t[p].lazy == INVALID) return ;
+                auto lazy = t[p].lazy; t[p].lazy = INVALID;
+
+                auto lc = p << 1, rc = lc | 1, mid = (cl + cr) >> 1;
+                merge (t[lc], lazy, mid - cl + 1);
+                merge (t[rc], lazy, cr - mid);
+            }
+
+        public:
+            sgt_lazy () : n(0), t(vector (0, Node (0, 0, 0))) {}
+            sgt_lazy (const vector <T> &a) {init(a);}
+            
+            void modify (int l, int r, T x) {modify (l, r, x, rt, 0, n - 1);}
+            Node qry (int l, int r) {return qry (l, r, rt, 0, n - 1);}
+
+            void init (const vector <T> &a) {
+                n = a.size();
+                t.resize(n << 2);
+                build (a, rt, 0, n - 1);
+            }
+        };
+
+        template <typename T>
+        class HPD {
+        public:
+            vector <int> dep, dfn, top, son, f, siz;
+            vector <T> s;
+            vector <vector <int>> e;
+            sgt_lazy <T> sgt;
+            int n, cnt;
+
+            HPD () : n(0), cnt(0), dep(vector (0, 0)), dfn(vector (0, 0)), top(vector (0, 0)), son(vector (0, 0)), 
+                f(vector (0, 0)), siz(vector (0, 0)), s(vector (0, T(0))), e(vector (0, vector (0, 0))) {}
+            HPD (int sz, int rt, const vector <vector <int>> &a, const vector <T> &c)  {init (sz, rt, a, c);}
+
+            void dfs1 (int v, int fa) {
+                dep[v] = dep[fa] + 1; f[v] = fa;
+                siz[v] = 1; son[v] = -1;
+                
+                int maxn = 0;
+                for (auto &u : e[v]) if (u != fa) {
+                    dfs1 (u, v);
+                    siz[v] += siz[u];
+                    if (maxn < siz[u]) maxn = siz[u], son[v] = u;
+                }
+            }
+
+            void dfs2 (int v, int head) {
+                dfn[v] = ++cnt;
+                top[v] = head;
+                if (son[v] != -1) dfs2 (son[v], head);
+
+                for (auto &u : e[v]) if (u != son[v] && u != f[v])
+                    dfs2(u, u);
+            }
+
+            void init (int sz, int rt, const vector <vector <int>> &a, const vector <T> &c) {
+                e = a, n = sz;
+                dep = dfn = top = son = f = siz = vector (n + 1, 0);
+                s.resize(n + 1);
+
+                dep[rt] = 1;
+                dfs1(rt, 0);
+                dfs2(rt, rt);
+
+                for (int i = 1; i <= n; i++) s[dfn[i]] = c[i];
+                sgt.init (s);
+            }
+
+            void modify (int v, int u, T x) {
+                while (top[v] != top[u]) {
+                    if (dep[top[v]] > dep[top[u]]) sgt.modify (dfn[top[u]], dfn[u], x), u = f[top[u]];
+                    else sgt.modify (dfn[top[v]], dfn[v], x), v = f[top[v]];
+                }
+
+                if (dep[v] > dep[u]) swap (v, u);
+                sgt.modify (dfn[v], dfn[u], x);
+            }
+
+            T qry (int v, int u) {
+                sgt_lazy :: Node <T> ans;
+                while (top[v] != top[u]) {
+                    if (dep[top[v]] > dep[top[u]]) ans = ans + sgt.qry (dfn[top[u]], dfn[u]), u = f[top[u]];
+                    else ans = ans + sgt.qry (dfn[top[v]], dfn[v]), v = f[top[v]];
+                }
+
+                if (dep[v] > dep[u]) swap (v, u);
+                ans += sgt.qry (dfn[v], dfn[u]);
+                return ans.ans;
+            }
+        };
     }
 }
 
-// using namespace K;
+using namespace K;
 // using namespace Hash;
 // using namespace math;
-// using namespace exstd;
+using namespace exstd;
 
 void init () {
     // init_fac ();
@@ -124,7 +269,30 @@ void init () {
 }
 
 void solve () {
-    
+    int n, m; cin >> n >> m;
+    vector a(n + 1, 0);
+    for (int i = 1; i <= n; i++) cin >> a[i];
+
+    vector e(n + 1, vector (0, 0));
+    for (int i = 1; i < n; i++) {
+        int v, u; cin >> v >> u;
+        e[v].push_back (u);
+        e[u].push_back (v);
+    }
+
+    HPD <int> K(n, 1, e, a);
+
+    for (int i = 1; i <= m; i++) {
+        char c; cin >> c;
+        if (c == 'Q') {
+            int v, u; cin >> v >> u;
+            cout << K.qry (v, u) << '\n';
+        }
+        else {
+            int v, u, x; cin >> v >> u >> x;
+            K.modify (v, u, x);
+        }
+    }
 }
 
 int main () {
@@ -133,6 +301,7 @@ int main () {
     freopen("my.out", "w", stdout);
 #endif
     ios :: sync_with_stdio(0);
+    // ios_base :: sync_with_stdio(0);
     cin.tie(0), cout.tie(0);
     // srand (time (0));
 
