7062f5321bd73e524ff084cb539c26f85963e21c
cqbzkrx <creesh114514@gmail.com>
2025-03-15 08:15:33 +0800

    Update TarJan (强连通分量).cpp

--- c++/模板/TarJan (强连通分量).cpp
+++ c++/模板/TarJan (强连通分量).cpp
@@ -1,36 +1,29 @@
-class tarjan {
-public:
-    static const int N = 5e4 + 7;
-    vector <int> dfn, low, vis;
-    vector <vector <int>> e, scc;
-    deque <int> s;
-    int num , id;
+vector dfn (n + 1, 0), low (n + 1, 0), vis (n + 1, 0);
+vector scc (n + 1, vector (0, 0));
+deque <int> s;
+int num = 0, id = 0;
 
-    void Tarjan(int x) {
-        dfn[x] = low[x] = ++num;
-        s.push_back(x); vis[x] = 1;
-        for (auto &v : e[x]) {
-            if (!dfn[v]) {
-                Tarjan(v);
-                low[x] = min(low[x], low[v]);
-            }
-            else if (vis[v]) low[x] = min(low[x], dfn[v]);
+auto tarjan = [&](auto &&self, int x) -> void {
+    dfn[x] = low[x] = ++num;
+    s.push_back (x); vis[x] = 1;
+    for (auto &v : e[x]) {
+        if (!dfn[v]) {
+            self, (self, v);
+            low[x] = min (low[x], low[v]);
         }
-
-        if (low[x] == dfn[x]) {
-            bool f = 0; id++;
-            while (s.back() != x) {
-                scc[id].push_back(s.back());
-                vis[s.back()] = 0;
-                s.pop_back();
-                f = 1;
-            }
-            if (f == 0) id--;
-            scc[id].push_back(s.back());
-            s.pop_back();
-        }
+        else if (vis[v]) low[x] = min (low[x], dfn[v]);
     }
 
-    tarjan () : e(vector (N, vector (0, 0))), dfn(vector (N, 0)), low(vector (N, 0)),
-                scc(vector (N, vector (0, 0))), vis(vector (N, 0)), num(0) {}
+    if (low[x] == dfn[x]) {
+        bool f = 0; id++;
+        while (s.back () != x) {
+            scc[id].push_back(s.back());
+            vis[s.back ()] = 0;
+            s.pop_back ();
+            f = 1;
+        }
+        if (f == 0) id--;
+        scc[id].push_back (s.back ());
+        s.pop_back ();
+    }
 };
