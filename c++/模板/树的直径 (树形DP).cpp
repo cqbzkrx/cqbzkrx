81bc736b35ec77a23553bf0ef35cf5c3f8c3bece
cqbzkrx <creesh114514@gmail.com>
2025-03-28 19:04:25 +0800

    Update 树的直径 (树形DP).cpp

--- c++/模板/树的直径 (树形DP).cpp
+++ c++/模板/树的直径 (树形DP).cpp
@@ -4,10 +4,10 @@ auto dfs2 = [&](auto &&self, int v, int fa) -> pair<int, ll> {
     for (auto &[u, w] : e[v])
         if (u != fa) {
             auto tmp = self(self, u, v);
-            tmp.second += w;
-            if (tmp.second > maxfi.second)
+            tmp.y += w;
+            if (tmp.y > maxfi.y)
                 swap(tmp, maxfi);
-            if (tmp.second > maxse.second)
+            if (tmp.y > maxse.y)
                 swap(tmp, maxse);
         }
 
@@ -18,9 +18,9 @@ auto dfs2 = [&](auto &&self, int v, int fa) -> pair<int, ll> {
 dfs2(dfs2, 1, 0);
 ll maxi = 0, maxj = 0, maxn = 0;
 for (int i = 1; i <= n; i++)
-    if (maxn < dp[i].first.second + dp[i].second.second) {
-        maxn = dp[i].first.second + dp[i].second.second;
-        maxi = dp[i].first.first, maxj = dp[i].second.first;
+    if (maxn < dp[i].x.y + dp[i].y.y) {
+        maxn = dp[i].x.y + dp[i].y.y;
+        maxi = dp[i].x.x, maxj = dp[i].y.x;
     }
 cout << maxn << '\n';
 
