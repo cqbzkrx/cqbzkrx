2ed0674e623a0a9cb8e2a5114214bf5bbdabbc7d
cqbzkrx <creesh114514@gmail.com>
2025-03-29 15:07:27 +0800

    Update Floyd (最短路).cpp

--- c++/模板/Floyd (最短路).cpp
+++ c++/模板/Floyd (最短路).cpp
@@ -1,6 +1,6 @@
-vector e(n + 1, vector (n + 1, inf));
-
-void floyd (int n) {
+auto floyd = [&](const vector <vector <ll>> &e) -> vector <vector <ll>> {
+	vector s = e;
 	for (int k = 1; k <= n; k++) for (int i = 1; i <= n; i++)
-	for (int j = 1; j <= n; j++) e[i][j] = min(e[i][j], e[i][k] + e[k][j]);
-}
+		for (int j = 1; j <= n; j++) s[i][j] = min(e[i][j], s[i][k] + s[k][j]);
+	return s;
+};
