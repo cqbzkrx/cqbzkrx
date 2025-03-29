a86ba76bcd66f5370e9763733c97409a02ca5512
cqbzkrx <creesh114514@gmail.com>
2025-03-29 15:26:28 +0800

    Update kruskal (最小生成树).cpp

--- c++/模板/kruskal (最小生成树).cpp
+++ c++/模板/kruskal (最小生成树).cpp
@@ -1,9 +1,19 @@
-struct Node {int v, u; ll w; Node () : v (0), u (0), w (0) {}};
+struct Node {
+    int v, u; ll w;
+    Node () : v (0), u (0), w (0) {}
+    Node (int _v, int _u, ll _w) : v (_v), u (_u), w (_w) {}
+
+    bool operator < (const Node &b) const {
+        return w < b.w;
+    }
+};
+vector e (0, Node ());
+sort (all(e));
 
 auto kruskal = [&](const vector <Node> &e, DSU dsu) -> ll {
     ll ans = 0;
     int s = n - 1;
-    for (auto [v, u, w] : e) if (merge (v, u)) {
+    for (auto [v, u, w] : e) if (dsu.merge (v, u)) {
         ans += w, s--;
         if (s == 0) break;
     }
