37e6523eacc625f3db911f6e69100b1ff6fa04e7
cqbzkrx <creesh114514@gmail.com>
2025-04-12 15:41:14 +0800

    Update ST (ST表).cpp

--- c++/模板/ST (ST表).cpp
+++ c++/模板/ST (ST表).cpp
@@ -1,16 +1,20 @@
 template <typename T>
 class ST {
 public:
-    static constexpr int N = 1e5 + 7;
+    static constexpr int N = 2e5 + 7;
     T f[__lg(N) + 1][N];
     int n, lim;
+
+    T merge (T a, T b) {
+        return max (a, b);
+    }
 
     void init (const vector <T> &a) {
         n = a.size(), lim = __lg(n) + 1;
         for (int i = 0; i < n; i++) f[0][i] = a[i];
         for (int i = 1; i < lim; i++) for (int j = 0; j < n; j++) {
             if (j + (1 << i) > n) break;
-            f[i][j] = max (f[i - 1][j], f[i - 1][j + (1 << (i - 1))]);
+            f[i][j] = merge (f[i - 1][j], f[i - 1][j + (1 << (i - 1))]);
         }
     }
 
@@ -19,6 +23,6 @@ public:
 
     T qry (int l, int r) {
         int lg = __lg (r - l + 1);
-        return max(f[lg][l], f[lg][r - (1 << lg) + 1]);
+        return merge (f[lg][l], f[lg][r - (1 << lg) + 1]);
     }
 };
