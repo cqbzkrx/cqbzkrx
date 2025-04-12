b92fa6bfc8855bb8d9f114dce127f6548d5b8763
cqbzkrx <creesh114514@gmail.com>
2025-04-12 10:31:13 +0800

    Update LCA (TarJan).cpp

--- c++/模板/LCA (TarJan).cpp
+++ c++/模板/LCA (TarJan).cpp
@@ -1,32 +1,10 @@
 class LCA_tarjan {
-    class DSU_of_LCA {
-    public:
-        vector <int> fa;
-        void init (int n) {
-            fa.resize(n + 1);
-            iota (all(fa), 0);
-        }
-
-        int find (int x) {
-            return fa[x] == x ? x : fa[x] = find(fa[x]);
-        }
-
-        void merge (int v, int u) {
-            auto fv = find(v), fu = find(u);
-            if (fv == fu) return ;
-            fa[fv] = fu;
-        }
-
-        DSU_of_LCA () : fa(vector (0, 0)) {}
-        DSU_of_LCA (int n) {init (n);}
-    };
-
 public:
     vector <vector <int>> e;
     vector <vector <pair <int, int>>> eq;
     vector <int> vis, ans;
     int n, q;
-    DSU_of_LCA dsu;
+    DSU dsu;
     
     LCA_tarjan () : n(0), q(0) {}
     LCA_tarjan (int sz, int k) {init (sz, k);}
