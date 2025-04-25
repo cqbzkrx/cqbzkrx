d191f8925a6c2c58f9d241d56aa4244e71c44e28
cqbzkrx <creesh114514@gmail.com>
2025-04-25 20:51:31 +0800

    Update 线性筛.cpp

--- c++/模板/线性筛.cpp
+++ c++/模板/线性筛.cpp
@@ -3,7 +3,7 @@ bitset <N> np;
 
 auto init_prime = [&](int n = N - 1) -> void {
     // divi.resize (N);
-    np[1] = 1;
+    np[1] = np[0] = 1;
     for (int i = 2; i <= n; i++) {
         if (!np[i]){
             prime.push_back (i);
