177597df8a847efeedb632587699cdefd149998e
cqbzkrx <creesh114514@gmail.com>
2025-04-26 22:23:13 +0800

    Create 素数筛 (埃式筛).cpp

---
+++ c++/模板/数学/数论/素数筛 (埃式筛).cpp
@@ -0,0 +1,10 @@
+vector<int> prime;
+bitset <N> np;
+auto init_prime_Era = [&](int n = N - 1) {
+    np[0] = np[1] = 1;
+    for (int i = 2; i * i <= n; i++) if (!np[i])
+        for (int j = i * i; j <= n; j += i) np[j] = 1;
+
+    for (int i = 2; i <= n; i++) if (!np[i])
+        prime.push_back (i);
+};
