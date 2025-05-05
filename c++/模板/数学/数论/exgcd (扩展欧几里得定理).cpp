cac772006695f55487a1f2f7c8427037c2e98d77
cqbzkrx <creesh114514@gmail.com>
2025-05-05 15:55:07 +0800

    Create exgcd (扩展欧几里得定理).cpp

---
+++ c++/模板/数学/数论/exgcd (扩展欧几里得定理).cpp
@@ -0,0 +1,10 @@
+auto exgcd = [&](auto &&self, ll a, ll b) -> pair <ll, ll> {
+    if (b == 0) return {1ll, 0ll};
+    auto [x, y] = self (self, b, a % b);
+    auto v = x;
+    x = y;
+    y = v - a / b * y;
+    return {x, y};
+};
+
+// ax + by = 1 的整数解
