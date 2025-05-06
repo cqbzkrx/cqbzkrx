d43a7a3e8535c654c16da13047fecc0e20d3f772
cqbzkrx <creesh114514@gmail.com>
2025-05-06 22:32:46 +0800

    Update exgcd (扩展欧几里得定理).cpp

--- c++/模板/数学/数论/exgcd (扩展欧几里得定理).cpp
+++ c++/模板/数学/数论/exgcd (扩展欧几里得定理).cpp
@@ -1,10 +1,11 @@
 auto exgcd = [&](auto &&self, ll a, ll b) -> pair <ll, ll> {
     if (b == 0) return {1ll, 0ll};
     auto [x, y] = self (self, b, a % b);
-    auto v = x;
+    auto tmp = x;
     x = y;
-    y = v - a / b * y;
+    y = tmp - a / b * y;
     return {x, y};
 };
 
 // ax + by = gcd (a, b) 的整数解
+// x的最小正整数解要进行 x = (x % b + b) % b
