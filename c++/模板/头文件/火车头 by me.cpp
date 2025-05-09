1a9b4cd89ba44aaf81311926b6510fa115b36ef2
cqbzkrx <creesh114514@gmail.com>
2025-05-09 19:31:48 +0800

    Update 火车头 by me.cpp

--- c++/模板/头文件/火车头 by me.cpp
+++ c++/模板/头文件/火车头 by me.cpp
@@ -18,6 +18,43 @@ const int N = 2e6 + 7;
 const int M = 2e3 + 7;
 
 namespace K {
+    namespace int_128 {
+        std::ostream &operator<<(std::ostream &os, i128 n) {
+            if (n == 0) return os << 0;
+            std::string s;
+            while (n > 0) {
+                s += char('0' + n % 10);
+                n /= 10;
+            }
+            std::reverse(s.begin(), s.end());
+            return os << s;
+        }
+        
+        i128 toi128(const std::string &s) {
+            i128 n = 0;
+            for (auto c : s) n = n * 10 + (c - '0');
+            return n;
+        }
+        
+        i128 sqrti128(i128 n) {
+            i128 lo = 0, hi = 1E16;
+            while (lo < hi) {
+                i128 x = (lo + hi + 1) / 2;
+                if (x * x <= n) lo = x;
+                else hi = x - 1;
+            }
+            return lo;
+        }
+
+        i128 gcd(i128 a, i128 b) {
+            while (b) {
+                a %= b;
+                std::swap(a, b);
+            }
+            return a;
+        }
+    }
+
     namespace Hash {
         struct custom_hash {
             static uint64_t splitmix64 (uint64_t x) {
@@ -119,6 +156,7 @@ namespace K {
 // using namespace Hash;
 // using namespace input;
 // using namespace math;
+// using namespace int_128;
 
 void init () {
     // init_fac ();
