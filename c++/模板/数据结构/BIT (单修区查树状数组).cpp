f08c2fbb1ae175b8ba1b1b41770f0a79913c52a8
cqbzkrx <creesh114514@gmail.com>
2025-05-10 15:36:12 +0800

    Create BIT (单修区查树状数组).cpp

---
+++ c++/模板/数据结构/BIT (单修区查树状数组).cpp
@@ -0,0 +1,25 @@
+template <typename T = ll>
+class BIT {
+public:
+    int n;
+    vector <T> t;
+    
+    BIT (int sz = 0) : n (sz), t (sz + 1) {}
+    BIT (const vector <T> &a) : BIT ((int) a.size ()) {
+        for (int i = 1; i <= n; i++) modify (i, a[i - 1]);
+    }
+
+    void modify (int i, T x) {
+        for (; i <= n; i += math :: lowbit (i)) t[i] += x;
+    }
+
+    T qry (int r) {
+        T ans = 0;
+        for (; r > 0; r -= math :: lowbit (r)) ans += t[r];
+        return ans;
+    }
+    
+    T qry (int l, int r) {
+        return qry (r) - qry (l - 1);
+    }
+};
