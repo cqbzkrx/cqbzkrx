0217b12a8cd4623eb3181b0a34b911480d9f3ef2
cqbzkrx <creesh114514@gmail.com>
2025-05-13 20:31:49 +0800

    Update BIT (区修区查树状数组).cpp

--- c++/模板/数据结构/BIT (区修区查树状数组).cpp
+++ c++/模板/数据结构/BIT (区修区查树状数组).cpp
@@ -1,59 +1,62 @@
 namespace BIT {
-    template <typename T = ll>
-    class _BIT {
-    public:
-        int n;
-        vector <T> t;
-        
-        _BIT (int sz = 0) : n (sz), t (sz + 1) {}
-        _BIT (const vector <T> &a) : _BIT ((int) a.size ()) {
-            for (int i = 1; i <= n; i++) modify (i, a[i - 1]), modify (i + 1, -a[i - 1]);
-        }
+int lowbit (int i) {return (i & -i);}
 
-        void modify (int i, T x) {
-            for (; i <= n; i += math :: lowbit (i)) t[i] += x;
-        }
+template <typename T = ll>
+class _BIT {
+public:
+    int n;
+    vector <T> t;
+    
+    _BIT (int sz = 0) : n (sz), t (sz + 1) {}
+    _BIT (const vector <T> &a) : _BIT ((int) a.size ()) {
+        for (int i = 1; i <= n; i++) modify (i, a[i - 1]), modify (i + 1, -a[i - 1]);
+    }
 
-        T qry (int r) {
-            T ans = 0;
-            for (; r > 0; r -= math :: lowbit (r)) ans += t[r];
-            return ans;
-        }
+    void modify (int i, T x) {
+        for (; i <= n; i += lowbit (i)) t[i] += x;
+    }
 
-        T qry (int l, int r) {
-            return qry (r) - qry (l - 1);
-        }
-    };
+    T qry (int r) {
+        T ans = 0;
+        for (; r > 0; r -= lowbit (r)) ans += t[r];
+        return ans;
+    }
 
-    template <typename T = ll>
-    class BIT {
-    public:
-        _BIT <T> bit0, bit1;
-        vector <T> sum;
+    T qry (int l, int r) {
+        return qry (r) - qry (l - 1);
+    }
+};
 
-        BIT (int sz = 0) : bit0 (sz + 1), bit1 (sz + 1) {}
-        BIT (const vector <T> &a) : sum (a.size () + 1), bit0 ((int) a.size () + 1), bit1 ((int) a.size () + 1) {
-            for (int i = 1; i <= (int) a.size (); i++) sum[i] = sum[i - 1] + a[i - 1];
-        }
+template <typename T = ll>
+class BIT {
+public:
+    _BIT <T> bit0, bit1;
+    vector <T> sum;
 
-        void modify (int l, int r, T x) {
-            l++, r++;
-            bit0.modify (l, x);
-            bit0.modify (r + 1, -x);
-            bit1.modify (l, x * l);
-            bit1.modify (r + 1, -x * (r + 1));
-        }
+    BIT (int sz = 0) : bit0 (sz + 1), bit1 (sz + 1) {}
+    BIT (const vector <T> &a) : sum (a.size () + 1), bit0 ((int) a.size () + 1), bit1 ((int) a.size () + 1) {
+        for (int i = 1; i <= (int) a.size (); i++) sum[i] = sum[i - 1] + a[i - 1];
+    }
 
-        T qry (int l, int r) {
-            l++, r++;
-            auto calc = [&] (int r) -> T {
-                return sum[r] + (r + 1) * bit0.qry (r) - bit1.qry (r);
-            };
+    void modify (int l, int r, T x) {
+        if (l > r) return ;
+        l++, r++;
+        bit0.modify (l, x);
+        bit0.modify (r + 1, -x);
+        bit1.modify (l, x * l);
+        bit1.modify (r + 1, -x * (r + 1));
+    }
 
-            return calc (r) - calc (l - 1);
-        }
+    T qry (int l, int r) {
+        l++, r++;
+        auto calc = [&] (int r) -> T {
+            return sum[r] + (r + 1) * bit0.qry (r) - bit1.qry (r);
+        };
 
-        T qry (int l) {return qry (l, l);}
-        void modify (int l, T x) {modify (l, l, x);}
-    };
+        return calc (r) - calc (l - 1);
+    }
+
+    T qry (int l) {return qry (l, l);}
+    void modify (int l, T x) {modify (l, l, x);}
+};
 }
