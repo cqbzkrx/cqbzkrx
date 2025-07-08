0265765a261bc9801c8307dadcc7e4aecfe36344
cqbzkrx <creesh114514@gmail.com>
2025-07-08 10:55:08 +0800

    Update 01-BFS (最短路).cpp

--- c++/模板/图论/01-BFS (最短路).cpp
+++ c++/模板/图论/01-BFS (最短路).cpp
@@ -7,8 +7,8 @@ auto bfs = [&](int s) -> vector <int> {
         auto u = q.front(); q.pop_front();
         if (vis[u] == 1) continue; vis[u] = 1;
         for (auto &[v, w] : e[u]) if (!vis[v]) {
-            if (w == 0) dq.push_front (v);
-            else dq.push_back (v);
+            if (w == 0) q.push_front (v);
+            else q.push_back (v);
             dis[v] = dis[u] + w;
         }
     } 
