c6fbfb2b6ad0ff2224ab6f80f24cfd3195cbdf5e
cqbzkrx <creesh114514@gmail.com>
2025-07-08 10:55:17 +0800

    Update 01-BFS (最短路) (输出路径).cpp

--- c++/模板/图论/01-BFS (最短路) (输出路径).cpp
+++ c++/模板/图论/01-BFS (最短路) (输出路径).cpp
@@ -9,8 +9,8 @@ auto bfs = [&](int s) -> pair <vector <int>, vector <int>> {
         if (vis[u] == 1) continue; vis[u] = 1;
 
         for (auto &[v, w] : e[u]) if (!vis[v]) {
-            if (w == 0) dq.push_front (v);
-            else dq.push_back (v);
+            if (w == 0) q.push_front (v);
+            else q.push_back (v);
             dis[v] = dis[u] + w;
             fa[v] = u;
         }
