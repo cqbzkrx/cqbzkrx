3d020ec932bbff15b7217bd8e2f49f27b4fe64c8
cqbzkrx <creesh114514@gmail.com>
2025-07-14 09:49:05 +0800

    Create Trie (字典树).cpp

---
+++ c++/模板/字符串/Trie (字典树).cpp
@@ -0,0 +1,46 @@
+namespace Trie {
+    struct Node {
+        array <int, 26> son;
+        array <int, 26> cnt;
+        Node () {
+            fill (all(son), 0);
+            fill (all(cnt), 0);
+        }
+    };
+    
+    class Trie {
+    public:
+        static constexpr int rt = 0;
+
+        vector <Node> t;
+        int cnt = 0;
+
+        Trie () : t (1, Node ()) {}
+        Trie (const vector <string> &a)  : t (1, Node ()) {init (a);}
+
+        void init (const vector <string> &a) {
+            for (const string &str : a) insert (str);
+        }
+
+        void insert (const string &str) {
+            int p = rt;
+            for (auto ch : str) {
+                auto v = ch - 'a';
+                t[p].cnt[v]++;
+                if (t[p].son[v]) p = t[p].son[v];
+                else t.push_back (Node ()), p = t[p].son[v] = ++cnt;
+            }
+        }
+
+        bool qry (const string &str) {
+            int p = rt;
+            for (auto ch : str) {
+                auto v = ch - 'a';
+                if (!t[p].cnt[v]) return 0;
+                if (!t[p].son[v]) return 0;
+                p = t[p].son[v];
+            }
+            return 1;
+        }
+    };
+}
