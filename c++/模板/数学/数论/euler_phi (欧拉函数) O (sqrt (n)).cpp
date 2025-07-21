8979265a824ec223e9945d6790ad2922f411bc80
cqbzkrx <creesh114514@gmail.com>
2025-07-21 11:34:11 +0800

    Update euler_phi (欧拉函数) O (sqrt (n)).cpp

--- c++/模板/数学/数论/euler_phi (欧拉函数) O (sqrt (n)).cpp
+++ c++/模板/数学/数论/euler_phi (欧拉函数) O (sqrt (n)).cpp
@@ -1,3 +1,4 @@
+// 1 : 
 auto get_euler_phi = [&](int n) -> int {
     int ans = n;
     for (int i = 2; i * i <= n; i++) if (n % i == 0) {
@@ -5,5 +6,18 @@ auto get_euler_phi = [&](int n) -> int {
         while (n % i == 0) n /= i;
     }
     if (n > 1) ans = ans / n * (n - 1);
+    return ans;
+};
+
+// 2 : prime
+auto get_euler_phi = [&](int n) -> int {
+    int ans = n;
+    for (auto v : prime) {
+        if (v * v > n) break;
+        if (n % v == 0) ans -= ans / v;
+        while (n && n % v == 0) n /= v;
+    }
+
+    if (n > 1) ans -= ans / n;
     return ans;
 };
