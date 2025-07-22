fec1045c4b3d5fca9031744af776edc26eff076e
cqbzkrx <creesh114514@gmail.com>
2025-07-22 10:22:50 +0800

    Update euler_phi (欧拉函数) (筛法).cpp

--- c++/模板/数学/数论/euler_phi (欧拉函数) (筛法).cpp
+++ c++/模板/数学/数论/euler_phi (欧拉函数) (筛法).cpp
@@ -12,17 +12,17 @@ for (int i = 2; i <= n; i++) if (np[i] == 0) {
 }
 
 // 2 : 线性筛 O (n)
-vector phi (n + 1, 0), np (n + 1, 0), prime (0, 0);
+vector phi (N + 1, 0), np (N + 1, 0);
 np[0] = np[1] = phi[1] = 1;
-for (int i = 2; i <= n; i++) {
+for (int i = 2; i <= N; i++) {
     if (!np[i]) {
         prime.push_back (i);
         phi[i] = i - 1;
     }
     for (auto v : prime) {
-        if (i * v > n) break;
+        if (i * v > N) break;
         np[i * v] = 1;
-        if (i % p == 0) {phi[i * v] = phi[i] * p; break;}
-        else phi[i * v] = phi[i] * (p - 1);
+        if (i % v == 0) {phi[i * v] = phi[i] * v; break;}
+        else phi[i * v] = phi[i] * (v - 1);
     }
 }
