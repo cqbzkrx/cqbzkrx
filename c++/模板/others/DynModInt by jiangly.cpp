c24b752f29cb6564c117d4f794302da8341d778e
cqbzkrx <creesh114514@gmail.com>
2025-07-24 10:40:46 +0800

    Create DynModInt by jiangly.cpp

---
+++ c++/模板/others/DynModInt by jiangly.cpp
@@ -0,0 +1,128 @@
+struct Barrett {
+public:
+    Barrett(u32 m_) : m(m_), im((u64)(-1) / m_ + 1) {}
+ 
+    constexpr u32 mod() const {
+        return m;
+    }
+ 
+    constexpr u32 mul(u32 a, u32 b) const {
+        u64 z = a;
+        z *= b;
+        
+        u64 x = u64((u128(z) * im) >> 64);
+        
+        u32 v = u32(z - x * m);
+        if (m <= v) {
+            v += m;
+        }
+        return v;
+    }
+ 
+private:
+    u32 m;
+    u64 im;
+};
+ 
+template<u32 Id>
+struct DynModInt {
+public:
+    constexpr DynModInt() : x(0) {}
+    template<std::unsigned_integral T>
+    constexpr DynModInt(T x_) : x(x_ % mod()) {}
+    template<std::signed_integral T>
+    constexpr DynModInt(T x_) {
+        int v = x_ % int(mod());
+        if (v < 0) {
+            v += mod();
+        }
+        x = v;
+    }
+    
+    constexpr static void setMod(u32 m) {
+        bt = m;
+    }
+    
+    static u32 mod() {
+        return bt.mod();
+    }
+    
+    constexpr u32 val() const {
+        return x;
+    }
+    
+    constexpr DynModInt operator-() const {
+        DynModInt res;
+        res.x = (x == 0 ? 0 : mod() - x);
+        return res;
+    }
+    
+    constexpr DynModInt inv() const {
+        auto v = invGcd(x, mod());
+        assert(v.first == 1);
+        return v.second;
+    }
+    
+    constexpr DynModInt &operator*=(const DynModInt &rhs) & {
+        x = bt.mul(x, rhs.val());
+        return *this;
+    }
+    constexpr DynModInt &operator+=(const DynModInt &rhs) & {
+        x += rhs.val();
+        if (x >= mod()) {
+            x -= mod();
+        }
+        return *this;
+    }
+    constexpr DynModInt &operator-=(const DynModInt &rhs) & {
+        x -= rhs.val();
+        if (x >= mod()) {
+            x += mod();
+        }
+        return *this;
+    }
+    constexpr DynModInt &operator/=(const DynModInt &rhs) & {
+        return *this *= rhs.inv();
+    }
+    
+    friend constexpr DynModInt operator*(DynModInt lhs, const DynModInt &rhs) {
+        lhs *= rhs;
+        return lhs;
+    }
+    friend constexpr DynModInt operator+(DynModInt lhs, const DynModInt &rhs) {
+        lhs += rhs;
+        return lhs;
+    }
+    friend constexpr DynModInt operator-(DynModInt lhs, const DynModInt &rhs) {
+        lhs -= rhs;
+        return lhs;
+    }
+    friend constexpr DynModInt operator/(DynModInt lhs, const DynModInt &rhs) {
+        lhs /= rhs;
+        return lhs;
+    }
+    
+    friend constexpr std::istream &operator>>(std::istream &is, DynModInt &a) {
+        i64 i;
+        is >> i;
+        a = i;
+        return is;
+    }
+    friend constexpr std::ostream &operator<<(std::ostream &os, const DynModInt &a) {
+        return os << a.val();
+    }
+    
+    friend constexpr bool operator==(const DynModInt &lhs, const DynModInt &rhs) {
+        return lhs.val() == rhs.val();
+    }
+    friend constexpr std::strong_ordering operator<=>(const DynModInt &lhs, const DynModInt &rhs) {
+        return lhs.val() <=> rhs.val();
+    }
+    
+private:
+    u32 x;
+    static Barrett bt;
+};
+ 
+template<u32 Id>
+Barrett DynModInt<Id>::bt = 998244353;
