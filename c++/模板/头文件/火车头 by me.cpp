d1c8fea47a745ce54d146066848162ab44bc65a3
cqbzkrx <creesh114514@gmail.com>
2025-07-27 15:59:45 +0800

    Update and rename 火车头 by me (no namespace).cpp to 火车头 by me.cpp

--- c++/模板/头文件/火车头 by me.cpp
+++ c++/模板/头文件/火车头 by me.cpp
@@ -55,6 +55,18 @@ i128 gcd(i128 a, i128 b) {
     return a;
 }
 
+template <class T>
+constexpr vector <T> operator += (vector <T> &lhs, const vector <T> &rhs) {
+    lhs.insert (lhs.end (), all(rhs));
+    return lhs;
+}
+
+template <class T>
+constexpr vector <T> operator + (vector <T> lhs, const vector <T> &rhs) {
+    lhs += rhs;
+    return lhs;
+}
+
 template<typename T>
 void add (T &x, const T &y) {if ((x += y) >= mod) x -= mod;}
 template<typename T>
