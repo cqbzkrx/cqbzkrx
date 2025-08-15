f6592f7946c91dcee90ec7485d730dc91b557754
cqbzkrx <creesh114514@gmail.com>
2025-08-15 08:28:07 +0800

    Update TopoSort (拓扑排序).cpp

--- c++/模板/图论/TopoSort (拓扑排序).cpp
+++ c++/模板/图论/TopoSort (拓扑排序).cpp
@@ -1,11 +1,11 @@
 auto toposort = [&]() -> bool {
-    priority_queue <int, vector <int>, greater<int>> q;
+    queue <int> q;
     for (int i = 1; i <= n; i++) if (!in[i])
         q.push(i);
     int cnt = 0;
     while (q.size() != 0) {
         cnt++;
-        auto t = q.top(); q.pop();
+        auto t = q.back(); q.pop();
         ans.push_back(t);
         for (auto &v : e[t]) if (--in[v] == 0)
             q.push(v);
