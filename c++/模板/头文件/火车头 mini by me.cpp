d3a27b8366badd9c9ae34901afd99f604c74637b
cqbzkrx <creesh114514@gmail.com>
2025-08-17 17:59:10 +0800

    Create 火车头 mini by me.cpp

---
+++ c++/模板/头文件/火车头 mini by me.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+// #include <bits/extc++.h>
+// using namespace __gnu_cxx;
+// using namespace __gnu_pbds;
+#define all(x) x.begin (), x.end ()
+#define x first
+#define y second
+using namespace std;
+using ll = long long;
+using u64 = unsigned long long;
+using u32 = unsigned int;
+using db = double;
+using i128 = __int128;
+using u128 = unsigned __int128;
+using i64 = long long;
+const int inf = 1e9 + 7;
+const ll INF = 1e18 + 7;
+const db eps = 1e-6;
+const int mod = 998244353;
+const int N = 100 + 7;
+const int M = 2e3 + 7;
+
+void init () {
+    
+}
+
+void solve () {
+    
+}
+
+int main () {
+#ifndef ONLINE_JUDGE
+    freopen ("1.in", "r", stdin);
+    freopen ("my.out", "w", stdout);
+#endif
+    ios :: sync_with_stdio (0);
+    // ios_base :: sync_with_stdio (0);
+    cin.tie (0), cout.tie (0);
+    // srand (time (0));
+
+    init ();    
+
+    cout << setprecision (15) << fixed;
+    cerr << setprecision (2) << fixed;
+
+    int t = 1;
+    // cin >> t;
+    // read (t);
+    for (int i = 1; i <= t; i++) {
+        // cout << "Case "<< i << ": ";
+        solve ();
+    }
+}
