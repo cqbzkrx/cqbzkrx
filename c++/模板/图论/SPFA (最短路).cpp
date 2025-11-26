3b7ba14c24ca741b89fedc9335ed990082a67e50
cqbzkrx <creesh114514@gmail.com>
2025-11-26 19:30:09 +0800

    Update SPFA (最短路).cpp

--- c++/模板/图论/SPFA (最短路).cpp
+++ c++/模板/图论/SPFA (最短路).cpp
@@ -1,20 +1,19 @@
-vector dis (n + 1, INF);
-auto spfa = [&](int s) -> bool {
+auto spfa = [&](int s, const vector <vector <pair <int, ll>>> &e) -> pair <vector <ll>, bool> {
+    vector dis (n + 1, INF); dis[s] = 0;
     vector vis (n + 1, 0);
     // vector cnt (n + 1, 0);
-    fill (all(dis), INF); dis[s] = 0;
-    deque <int> dq; dq.push_back (s);
-    while (dq.size()) {
-        auto u = dq.front(); dq.pop_front();
+    queue <int> q; q.push (s);
+    while (q.size ()) {
+        auto u = q.front (); q.pop ();
         vis[u] = 0;
-        for (auto &[v, w] : e[u]) if (dis[u] + w < dis[v]) {
-            dis[v] = dis[u] + w;
-            if (!vis[v]) {
-                vis[v] = 1; dq.push_back (v);
+        for (auto [v, w] : e[u]) {
+            if (dis[v] > dis[u] + w) {
+                dis[v] = dis[u] + w;
+                if (!vis[v]) vis[v] = 1, q.push (v);
                 // cnt[v]++;
             }
-            // if (cnt[v] > n) return 0;
+            // if (cnt[v] > n) return {vector (0, 0ll), 0};
         }
     }
-    return 1;
+    return {dis, 1};
 };
