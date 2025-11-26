b99696140822fa980129fed9b26dcece8845e7fd
cqbzkrx <creesh114514@gmail.com>
2025-11-26 19:10:47 +0800

    Update dijkstra (最短路).cpp

--- c++/模板/图论/dijkstra (最短路).cpp
+++ c++/模板/图论/dijkstra (最短路).cpp
@@ -1,16 +1,13 @@
-auto dijkstra = [&](int s) -> vector <ll> {
+auto dij = [&](int s, const vector <vector <pair <int, ll>>> &e) -> vector <ll> {
     vector dis (n + 1, INF); dis[s] = 0;
     vector vis (n + 1, 0);
-    priority_queue <pair <ll, int>, vector <pair <ll, int>>, greater <pair <ll, int>>> pq;
-    pq.push ({0ll, s});
-
-    while (pq.size ()) {
-        auto u = pq.top().y; pq.pop();
+    using T = pair <ll, int>;
+    priority_queue <T, vector <T>, greater <T>> q; q.emplace (0ll, s);
+    while (q.size ()) {
+        auto [_w, u] = q.top (); q.pop ();
         if (vis[u]) continue; vis[u] = 1;
-        
-        for (auto &[v, w] : e[u]) if (dis[u] + w < dis[v])
-            dis[v] = dis[u] + w, pq.push ({dis[v], v});
+        for (auto [v, w] : e[u]) if (dis[v] > dis[u] + w)
+            dis[v] = dis[u] + w, q.emplace (dis[v], v);
     }
-
     return dis;
 };
