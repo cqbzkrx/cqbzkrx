45089a00c7a4653d1616aaacb9ccd288614ed25f
cqbzkrx <creesh114514@gmail.com>
2025-11-26 16:41:22 +0800

    Refactor segment tree implementation for clarity

--- c++/模板/数据结构/segtree (动态开点+区修区查线段树).cpp
+++ c++/模板/数据结构/segtree (动态开点+区修区查线段树).cpp
@@ -1,101 +1,77 @@
-namespace segment_tree {
+namespace sgt {
     static constexpr ll INVALID = -1;
 
-    template <typename T = ll>
+    template <typename T>
     struct Node {
-        int l, r;
         T val, lazy;
-        Node (T _val = 0, int _l = -1, int _r = -1, T _lazy = INVALID) : 
-            val (_val), l (_l), r (_r), lazy (_lazy) {}
+        int lc, rc;
+        Node (T _val = 0, int _lc = -1, int _rc = -1, T _lazy = INVALID) : val (_val), lc (_lc), rc (_rc), lazy (_lazy) {}
+        Node operator + (const Node &rhs) const {
+            Node ret;
+            ret.val = val + rhs.val;
+            return ret;
+        }
     };
 
-    template <typename T = ll, class info = Node <T>>
+    template <typename T, class info = Node <T>>
     class segtree {
     public:
         static constexpr int rt = 0;
-
         vector <info> t;
-        int n, cnt;
+        int n, num;
 
-        segtree (int _n = 0) : cnt (0) {init (_n);}
-        segtree (const vector <T> &a) : cnt (0) {init (a);}
-
-        void init (int _n = 0) {
-            n = _n;
-            t.push_back (info ());
-        }
-
-        void init (const vector <T> &a) {
-            n = a.size ();
-            t.reserve (n + 1 << 2);
-            t.push_back (info ());
-            build (a, rt, 0, n - 1);
+        segtree (int n = 0, T x = 0) {init (n, x);}
+        void init (int n, T x) {
+            this -> n = n;
+            num = 0;
+            t.assign (1, info (x * n, -1, -1, x));
         }
 
         int insert () {
-            cnt++;
             t.push_back (info ());
-            return cnt;
-        }
-
-        void build (const vector <T> &a, int p, int cl, int cr) {
-            if (cl == cr) {t[p].val = a[cl]; return ;}
-            if (t[p].l == -1) t[p].l = insert ();
-            if (t[p].r == -1) t[p].r = insert ();
-            auto lc = t[p].l, rc = t[p].r, mid = (cl + cr) >> 1;
-            build (a, lc, cl, mid), build (a, rc, mid + 1, cr);
-            merge (p, lc, rc);
+            return ++num;
         }
 
         void merge (int p, int lc, int rc) {
-            t[p].val = t[lc].val + t[rc].val;
-        }
-
-        info merge (const info &a, const info &b) {
-            info res;
-            res.val = a.val + b.val;
-            return res;
+            t[p] = t[lc] + t[rc];
+            t[p].lc = lc; t[p].rc = rc;
         }
 
         void get (info &s, T x, int len = 1) {
-            s.val += x * len;
+            s.val = s.val + x * len;
             s.lazy = (s.lazy == INVALID ? 0 : s.lazy) + x;
         }
 
         void push_down (int p, int cl, int cr) {
-            if (cl == cr) return ;
-            auto lazy = t[p].lazy; t[p].lazy = INVALID;
-
-            if (t[p].l == -1) t[p].l = insert ();
-            if (t[p].r == -1) t[p].r = insert ();
-            auto lc = t[p].l, rc = t[p].r, mid = (cl + cr) >> 1;
-
-            if (lazy == INVALID) return ;
-            get (t[lc], lazy, mid - cl + 1);
-            get (t[rc], lazy, cr - mid);
+            if (t[p].lazy == INVALID || cl == cr) return ;
+            if (t[p].lc == -1) t[p].lc = insert ();
+            if (t[p].rc == -1) t[p].rc = insert ();
+            auto lc = t[p].lc, rc = t[p].rc, mid = (cl + cr) >> 1;
+            get (t[lc], t[p].lazy, mid - cl + 1);
+            get (t[rc], t[p].lazy, cr - mid);
+            t[p].lazy = INVALID;
         }
 
+        void modify (int i, T x) {modify (i, i, x, rt, 0, n - 1);}
+        void modify (int l, int r, T x) {modify (l, r, x, rt, 0, n - 1);}
         void modify (int l, int r, T x, int p, int cl, int cr) {
             if (cl > r || cr < l) return ;
             if (cl >= l && cr <= r) {get (t[p], x, cr - cl + 1); return ;}
             push_down (p, cl, cr);
-            auto lc = t[p].l, rc = t[p].r, mid = (cl + cr) >> 1;
+            auto lc = t[p].lc, rc = t[p].rc, mid = (cl + cr) >> 1;
             modify (l, r, x, lc, cl, mid);
             modify (l, r, x, rc, mid + 1, cr);
             merge (p, lc, rc);
         }
 
+        info qry (int i) {return qry (i, i, rt, 0, n - 1);}
+        info qry (int l, int r) {return qry (l, r, rt, 0, n - 1);}
         info qry (int l, int r, int p, int cl, int cr) {
             if (cl > r || cr < l) return info ();
             if (cl >= l && cr <= r) return t[p];
             push_down (p, cl, cr);
-            auto lc = t[p].l, rc = t[p].r, mid = (cl + cr) >> 1;
-            return merge (qry (l, r, lc, cl, mid), qry (l, r, rc, mid + 1, cr));
+            auto lc = t[p].lc, rc = t[p].rc, mid = (cl + cr) >> 1;
+            return qry (l, r, lc, cl, mid) + qry (l, r, rc, mid + 1, cr);
         }
-
-        void modify (int l, int r, T x) {modify (l, r, x, rt, 0, n - 1);}
-        void modify (int i, T x) {modify (i, i, x, rt, 0, n - 1);}
-        info qry (int l, int r) {return qry (l, r, rt, 0, n - 1);}
-        info qry (int i) {return qry (i, i, rt, 0, n - 1);}
     };
 }
