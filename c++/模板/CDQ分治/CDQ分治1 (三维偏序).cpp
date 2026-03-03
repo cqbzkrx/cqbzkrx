d6c9fe2782d9fa3fc27b3755bf60094e08a65d5f
cqbzkrx <creesh114514@gmail.com>
2026-03-03 17:22:04 +0800

    Update CDQ分治1 (三维偏序).cpp

--- c++/模板/CDQ分治/CDQ分治1 (三维偏序).cpp
+++ c++/模板/CDQ分治/CDQ分治1 (三维偏序).cpp
@@ -1,6 +1,41 @@
 // https://www.luogu.com.cn/problem/P3810
 // https://www.luogu.com.cn/record/265152141
 
+template <typename T = ll>
+class BIT {
+public:
+    int lowbit (int i) {return (i & -i);}
+
+    vector <T> t;
+    
+    BIT (int sz = 0) : t (sz + 10) {}
+    BIT (const vector <T> &a) : BIT ((int) a.size ()) {
+        for (int i = 0; i < a.size (); i++) modify (i, a[i]);
+    }
+
+    void modify (int x, T v) {
+        x++;
+        while (x < t.size ()) {
+            t[x] += v;
+            x += lowbit(x);
+        }
+    }
+
+    T qry (int x) {
+        x++;
+        T ans = 0;
+        while (x > 0) {
+            ans += t[x];
+            x -= lowbit(x);
+        }
+        return ans + t[0];
+    }
+    
+    T qry (int l, int r) {
+        return qry (r) - qry (l - 1);
+    }
+};
+
 void solve () {
     int n, k; cin >> n >> k;
     vector a (n, tuple (0, 0, 0, 0));
