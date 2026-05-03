194c967abf226382af7c120b74495f129da40e72
cqbzkrx <creesh114514@gmail.com>
2026-05-03 15:17:46 +0800

    Update SAM (后缀自动机) (endpos_size).cpp

--- c++/模板/字符串/SAM (后缀自动机) (endpos_size).cpp
+++ c++/模板/字符串/SAM (后缀自动机) (endpos_size).cpp
@@ -5,7 +5,7 @@ namespace SAM {
     struct Node {
         array <int, MAXM> ch;
         int fa, len, siz;
-        Node () : fa (-1), len (0), siz (0) {fill (all(ch), -1);}
+        Node () : fa (0), len (0), siz (0) {fill (all(ch), 0);}
     };
 
     inline int get (char c) {
@@ -15,12 +15,13 @@ namespace SAM {
     template <class info = Node>
     class SAM {
     public:
-        static constexpr int rt = 0;
+        static constexpr int rt = 1;
         vector <info> t;
         int lst, tot;
 
-        SAM () : lst (0), tot (0) {
+        SAM () : lst (1), tot (1) {
             t.reserve (MAXN);
+            t.emplace_back ();
             t.emplace_back ();
         }
         SAM (const string &str) : SAM () {Insert (str);}
@@ -39,16 +40,16 @@ namespace SAM {
 
             int p = lst, np = lst = new_node ();
             t[np].len = t[p].len + 1; t[np].siz = 1;
-            for (; p != -1 && t[p].ch[v] == -1; p = t[p].fa) t[p].ch[v] = np;
+            for (; p && !t[p].ch[v]; p = t[p].fa) t[p].ch[v] = np;
 
-            if (p == -1) {t[np].fa = rt; return ;}
+            if (!p) {t[np].fa = rt; return ;}
             int q = t[p].ch[v];
             if (t[p].len + 1 == t[q].len) {t[np].fa = q; return ;}
 
             int nq = new_node ();
             t[nq] = t[q]; t[nq].siz = 0; t[nq].len = t[p].len + 1;
             t[q].fa = t[np].fa = nq;
-            for (; p != -1 && t[p].ch[v] == q; p = t[p].fa) t[p].ch[v] = nq;
+            for (; p && t[p].ch[v] == q; p = t[p].fa) t[p].ch[v] = nq;
         }
 
         inline info & operator [] (int i) {return t[i];}
@@ -61,15 +62,15 @@ namespace SAM {
         vector <vector <int>> e;
         void init_pt () {
             e.assign (tot + 1, vector <int> (0, 0));
-            for (int i = 1; i <= tot; i++) e[t[i].fa].push_back (i);
+            for (int i = 2; i <= tot; i++) e[t[i].fa].push_back (i);
         }
 
         void calc_siz () {
             vector <int> cnt (tot + 1, 0), id (tot + 1, 0);
-            for (int i = 1; i <= tot; i++) cnt[t[i].len]++;
+            for (int i = 2; i <= tot; i++) cnt[t[i].len]++;
             for (int i = 1; i <= tot; i++) cnt[i] += cnt[i - 1];
-            for (int i = 1; i <= tot; i++) id[cnt[t[i].len]--] = i;
-            for (int i = tot; i >= 1; i--) t[t[id[i]].fa].siz += t[id[i]].siz;
+            for (int i = 2; i <= tot; i++) id[cnt[t[i].len]--] = i;
+            for (int i = tot; i >= 2; i--) t[t[id[i]].fa].siz += t[id[i]].siz;
         }
     };
 }
