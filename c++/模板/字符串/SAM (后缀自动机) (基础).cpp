8a4c73da3776e7cb8398cd7dc4767c31062390c9
cqbzkrx <creesh114514@gmail.com>
2026-05-04 08:45:52 +0800

    Refactor SAM class and update node initialization

--- c++/模板/字符串/SAM (后缀自动机) (基础).cpp
+++ c++/模板/字符串/SAM (后缀自动机) (基础).cpp
@@ -1,26 +1,28 @@
 namespace SAM {
-    static constexpr int MAXN = 2e6 + 7;  // 2 * N
+    static constexpr int MAXN = 1e6 + 7;
     static constexpr int MAXM = 26;
+    static constexpr int A = 'a';
 
     struct Node {
         array <int, MAXM> ch;
         int fa, len;
-        Node () : fa (-1), len (0) {fill (all(ch), -1);}
+        Node () : fa (0), len (0) {fill (all(ch), 0);}
     };
 
     inline int get (char c) {
-        return c - 'a';
+        return c - A;
     }
 
     template <class info = Node>
     class SAM {
     public:
-        static constexpr int rt = 0;
+        static constexpr int rt = 1;
         vector <info> t;
         int lst, tot;
 
-        SAM () : lst (0), tot (0) {
-            t.reserve (MAXN);
+        SAM () : lst (1), tot (1) {
+            t.reserve (MAXN << 1);
+            t.emplace_back ();
             t.emplace_back ();
         }
         SAM (const string &str) : SAM () {Insert (str);}
@@ -39,28 +41,28 @@ namespace SAM {
 
             int p = lst, np = lst = new_node ();
             t[np].len = t[p].len + 1;
-            for (; p != -1 && t[p].ch[v] == -1; p = t[p].fa) t[p].ch[v] = np;
+            for (; p && !t[p].ch[v]; p = t[p].fa) t[p].ch[v] = np;
 
-            if (p == -1) {t[np].fa = rt; return ;}
+            if (!p) {t[np].fa = rt; return ;}
             int q = t[p].ch[v];
             if (t[p].len + 1 == t[q].len) {t[np].fa = q; return ;}
 
             int nq = new_node ();
             t[nq] = t[q]; t[nq].len = t[p].len + 1;
             t[q].fa = t[np].fa = nq;
-            for (; p != -1 && t[p].ch[v] == q; p = t[p].fa) t[p].ch[v] = nq;
+            for (; p && t[p].ch[v] == q; p = t[p].fa) t[p].ch[v] = nq;
         }
 
         inline info & operator [] (int i) {return t[i];}
         inline const info operator [] (int i) const {return t[i];}
-        inline int fa (int i) {return t[i].fa;}
-        inline int ch (int i, int j) {return t[i].ch[j];}
-        inline int len (int i) {return t[i].len;}
+        inline int & fa (int i) {return t[i].fa;}
+        inline int & ch (int i, int j) {return t[i].ch[j];}
+        inline int & len (int i) {return t[i].len;}
 
         vector <vector <int>> e;
         void init_pt () {
             e.assign (tot + 1, vector <int> (0, 0));
-            for (int i = 1; i <= tot; i++) e[t[i].fa].push_back (i);
+            for (int i = 2; i <= tot; i++) e[t[i].fa].push_back (i);
         }
     };
 }
