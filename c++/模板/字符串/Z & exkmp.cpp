707b9e3da493af8398ff23c940bdcffde06dd7b9
cqbzkrx <creesh114514@gmail.com>
2026-05-08 21:27:07 +0800

    Update Z & exkmp.cpp

--- c++/模板/字符串/Z & exkmp.cpp
+++ c++/模板/字符串/Z & exkmp.cpp
@@ -1,11 +1,10 @@
 auto get_z = [&](const string &s, vector <int> &z) -> void {
-    int n = s.size ();
-    z.assign (n, 0);
+    z.resize (s.size ()); z[0] = 0;
 
     int l = 0, r = 0;
-    for (int i = 1; i < n; i++) {
+    for (int i = 1; i < s.size (); i++) {
         if (i < r) z[i] = min (r - i, z[i - l]);
-        while (i + z[i] < n && s[z[i]] == s[i + z[i]]) z[i]++;
+        while (i + z[i] < s.size () && s[z[i]] == s[i + z[i]]) z[i]++;
         if (i + z[i] > r) l = i, r = i + z[i];
     }
 };
