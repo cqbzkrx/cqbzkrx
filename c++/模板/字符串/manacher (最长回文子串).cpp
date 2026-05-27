72f04e664bb516468e171c0488c87de0d4f41339
cqbzkrx <creesh114514@gmail.com>
2026-05-27 23:39:30 +0800

    Update manacher (最长回文子串).cpp

--- c++/模板/字符串/manacher (最长回文子串).cpp
+++ c++/模板/字符串/manacher (最长回文子串).cpp
@@ -1,18 +1,16 @@
-auto manacher = [&](const string &s) -> int {
-    string a = "$";
-    for (int i = 0; i < (int) s.size (); i++) a += "#", a += s[i];
-    a += "#@";
+auto manacher = [&](const string &s, vector <int> &p) -> void {
+    string t = "#";
+    for (auto c : s) t += c, t += '#';
 
-    int siz = a.size (), mid = 0, r = 0;
-    vector <int> p (siz, 0);
-    for (int i = 0; i < siz; i++) {
-        if (i <= r) p[i] = min (r - i + 1, p[(mid << 1) - i]);
-        while (i - p[i] >= 0 && i + p[i] < siz && a[i - p[i]] == a[i + p[i]])
+    int n = t.size ();
+    p.resize (n);
+
+    int m = 0, r = 0;
+    for (int i = 0; i < n; i++) {
+        if (i <= r) p[i] = min (r - i + 1, p[(m << 1) - i]);
+        else p[i] = 1;
+        while (i - p[i] >= 0 && i + p[i] < n && t[i - p[i]] == t[i + p[i]])
             p[i]++;
-        if (i + p[i] - 1 >= r) mid = i, r = i + p[i] - 1;
+        if (i + p[i] - 1 > r) m = i, r = i + p[i] - 1;
     }
-
-    int ans = 0;
-    for (int i = 0; i < siz; i++) cmax (ans, p[i] - 1);
-    return ans;
 };
