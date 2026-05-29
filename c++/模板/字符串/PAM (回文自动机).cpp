fbaa5e0134db67ef2fdda2608e9ea6e451098d74
cqbzkrx <creesh114514@gmail.com>
2026-05-29 21:34:11 +0800

    Create PAM (回文自动机).cpp

---
+++ c++/模板/字符串/PAM (回文自动机).cpp
@@ -0,0 +1,58 @@
+namespace PAM {
+    static constexpr int MAXN = 1e6 + 7;
+    static constexpr int MAXM = 26;
+    static constexpr int A = 'a';
+
+    struct Node {
+        array <int, MAXM> ch;
+        int fail, cnt, len;
+        Node (int _len = 0) : len (_len), cnt (0), fail (0) {fill (all(ch), 0);}
+    };
+
+    inline int get (char c) {
+        return c - A;
+    }
+
+    template <class info = Node>
+    class PAM {
+    public:
+        vector <info> t;
+        string s;
+        int num, lst;
+
+        PAM () : num (1), lst (0) {
+            t.reserve (MAXN);
+            t.emplace_back (0);
+            t.emplace_back (-1);
+            t[0].fail = t[1].fail = 1;
+            s = string ();
+        }
+        PAM (const string &s) : PAM () {Insert (s);}
+
+        inline int new_node (int len = 0) {
+            t.emplace_back (len);
+            return ++num;
+        }
+
+        void Insert (const string &s) {
+            for (auto v : s) Insert (v);
+        }
+
+        int find (int p) {
+            while (s.size () - t[p].len - 1 <= 0 || *(s.rbegin () + t[p].len + 1) != *(s.rbegin ())) p = t[p].fail;
+            return p;
+        }
+
+        void Insert (char c) {
+            s += c;
+            int v = get (c), p = find (lst);
+            if (!t[p].ch[v]) {
+                int np = new_node (t[p].len + 2);
+                t[np].fail = t[find (t[p].fail)].ch[v];
+                t[p].ch[v] = np;
+                t[np].cnt = t[t[np].fail].cnt + 1;
+            }
+            lst = t[p].ch[v];
+        }
+    };
+}
