9780c07d557a55017ad3e1650a83fae7e31e632a
cqbzkrx <creesh114514@gmail.com>
2026-05-31 23:26:48 +0800

    Update PAM (回文自动机).cpp

--- c++/模板/字符串/PAM (回文自动机).cpp
+++ c++/模板/字符串/PAM (回文自动机).cpp
@@ -1,7 +1,6 @@
 namespace PAM {
     static constexpr int MAXN = 1e6 + 7;
     static constexpr int MAXM = 26;
-    static constexpr int A = 'a';
 
     struct Node {
         array <int, MAXM> ch;
@@ -10,7 +9,7 @@ namespace PAM {
     };
 
     inline int get (char c) {
-        return c - A;
+        return c - 'a';
     }
 
     template <class info = Node>
