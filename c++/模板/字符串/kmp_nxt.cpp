52b57f53151f7f31697ab3e9cfd236e9fd876e44
cqbzkrx <creesh114514@gmail.com>
2026-05-31 13:03:44 +0800

    Create kmp_nxt.cpp

---
+++ c++/模板/字符串/kmp_nxt.cpp
@@ -0,0 +1,8 @@
+auto get_nxt = [&](const string &c, vector <int> &nxt) -> void {
+    nxt.resize (c.size() + 1); nxt[0] = -1;
+    int i = 0, j = -1;
+    while (i < (int) c.size()) {
+        if (j == -1 || c[i] == c[j]) i++, j++, nxt[i] = j;
+        else j = nxt[j];
+    }
+};
