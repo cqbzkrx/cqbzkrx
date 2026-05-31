c87d4a65cd9341d3d893a26d4bdedf206243eacb
cqbzkrx <creesh114514@gmail.com>
2026-05-31 13:16:32 +0800

    Update KMP (KMP).cpp

--- c++/模板/字符串/KMP (KMP).cpp
+++ c++/模板/字符串/KMP (KMP).cpp
@@ -1,20 +1,25 @@
-auto get_nxt = [&](const string &c, vector <int> &nxt) -> void {
+auto get_nxt = [&](const string &c, vector <int> &nxt) -> void {   // != Border
     nxt.resize (c.size() + 1); nxt[0] = -1;
     int i = 0, j = -1;
     while (i < (int) c.size()) {
-        if (j == -1 || c[i] == c[j]) i++, j++, nxt[i] = j;
+        if (j == -1 || c[i] == c[j]) {
+            i++, j++;
+            if (c[i] == c[j]) nxt[i] = nxt[j];
+            else nxt[i] = j;
+        }
         else j = nxt[j];
     }
 };
 
-auto kmp = [&](const string &c, const string &s, const vector <int> nxt) -> pair <int, int> { // c中找s; first -> T/F, second -> idx
+auto kmp = [&](const string &c, const string &s, const vector <int> nxt) -> vector <int> {   // c中找s; nxt -> s_nxt
     int i = 0, j = 0;
+    vector ans (0, 0);
     while (i < (int) c.size()) {
         if (j == -1 || c[i] == s[j]) {
             i++, j++;
-            if (j == (int) s.size()) return {1, i - s.size()};
+            if (j == (int) s.size()) ans.push_back (i - s.size ()), j = nxt[j];
         }
         else j = nxt[j];
     }
-    return {-1, -1};
+    return ans;
 };
